66dc2e5fc5add4550ae3cd46aecd61c724427b98
Josh Frank <eurosewusa@gmail.com>
2021-03-19 23:04:58 -0400

    tweaks to styling before attempting drag-n-drop

--- src/components/CreateListingPage.js
+++ src/components/CreateListingPage.js
@@ -1,7 +1,7 @@
 // import { useSelector } from "react-redux";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { Container, Divider, Dropdown, Form, Header, Input, Segment, TextArea } from "semantic-ui-react";
+import { Button, Container, Divider, Dropdown, Form, Header, Input, Label, Segment, TextArea } from "semantic-ui-react";
 
 function CreateListingPage() {
 
@@ -9,21 +9,6 @@ function CreateListingPage() {
     if ( !localStorage.getItem( "token" ) ) history.push( "/" );
 
     // const currentUser = useSelector( state => state.currentUser );
-
-    const unitDropdownOptions = [
-        { key: 1, text: "Each", value: "each" },
-        { key: 2, text: "Spool", value: "each" },
-        { key: 3, text: "Roll", value: "each" },
-        { key: 4, text: "Bolt", value: "each" },
-        { key: 5, text: "Inch", value: "each" },
-        { key: 6, text: "Yard", value: "each" },
-        { key: 7, text: "Centimeter", value: "each" },
-        { key: 8, text: "Meter", value: "each" },
-        { key: 9, text: "Ounce", value: "each" },
-        { key: 10, text: "Pound", value: "each" },
-        { key: 11, text: "Gram", value: "each" },
-        { key: 12, text: "Kilogram", value: "each" }
-    ];
 
     const [ newListingFormState, setNewListingFormState ] = useState( {} );
     console.log('newListingFormState: ', newListingFormState);
@@ -45,6 +30,7 @@ function CreateListingPage() {
             <Header size="huge">Create new listing</Header>
             <Segment.Group>
                 <Segment>
+                    <Label>Listing title</Label>
                     <Input
                         fluid
                         size="massive"
@@ -57,6 +43,7 @@ function CreateListingPage() {
             <Segment.Group horizontal>
                 <Segment>
                     <Form>
+                        <Label>Listing description</Label>
                         <TextArea
                             rows="20"
                             name="description"
@@ -66,55 +53,82 @@ function CreateListingPage() {
                     </Form>
                 </Segment>
                 <Segment>
-                    <Input
-                        type="number"
-                        min="0"
-                        name="price"
-                        placeholder="Asking price"
-                        label="$"
-                        onChange={ updateNewListingForm }
-                    />
-                    <span style={ { fontSize: "16pt" } }> / </span>
-                    <Dropdown
-                        selection
-                        name="unit"
-                        placeholder='Unit'
-                        clearable={ true }
-                        options={ unitDropdownOptions }
-                        onChange={ ( changeEvent, { value } ) => updateNewListingFormUnit( value ) }
-                    />
-                    {/* <Dropdown
-                        name="unit"
-                        placeholder='Unit'
-                        clearable={ true }
-                        onChange={ updateNewListingForm }
-                    >
-                        <Dropdown.Menu>
-                            <Dropdown.Header content='Unit' />
-                            <Dropdown.Item>each</Dropdown.Item>
-                            <Dropdown.Item>spool</Dropdown.Item>
-                            <Dropdown.Item>roll</Dropdown.Item>
-                            <Dropdown.Item>bolt</Dropdown.Item>
-                            <Dropdown.Divider />
-                            <Dropdown.Header content='Length' />
-                            <Dropdown.Item>inch</Dropdown.Item>
-                            <Dropdown.Item>yard</Dropdown.Item>
-                            <Dropdown.Item>centimeter</Dropdown.Item>
-                            <Dropdown.Item>meter</Dropdown.Item>
-                            <Dropdown.Divider />
-                            <Dropdown.Header content='Weight' />
-                            <Dropdown.Item>pound</Dropdown.Item>
-                            <Dropdown.Item>ounce</Dropdown.Item>
-                            <Dropdown.Item>gram</Dropdown.Item>
-                            <Dropdown.Item>kilogram</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown> */}
+                    <Form.Group inline>
+                        <Input
+                            type="number"
+                            min="0"
+                            name="price"
+                            placeholder="Asking price"
+                            label="Price: $"
+                            onChange={ updateNewListingForm }
+                        />
+                        <span style={ { fontSize: "16pt" } }> / </span>
+                        <Dropdown
+                            selection
+                            name="unit"
+                            placeholder='Unit (leave blank if none)'
+                            clearable={ true }
+                            options={ [
+                                { key: 1, text: "Each", value: "each" },
+                                { key: 2, text: "Spool", value: "spool" },
+                                { key: 3, text: "Roll", value: "roll" },
+                                { key: 4, text: "Bolt", value: "bolt" },
+                                { key: 5, text: "Inch", value: "inch" },
+                                { key: 6, text: "Yard", value: "yard" },
+                                { key: 7, text: "Centimeter", value: "centimeter" },
+                                { key: 8, text: "Meter", value: "meter" },
+                                { key: 9, text: "Ounce", value: "ounce" },
+                                { key: 10, text: "Pound", value: "pound" },
+                                { key: 11, text: "Gram", value: "gram" },
+                                { key: 12, text: "Kilogram", value: "kilogram" }
+                            ] }
+                            onChange={ ( changeEvent, { value } ) => updateNewListingFormUnit( value ) }
+                        />
+                        <Input
+                            type="number"
+                            min="1"
+                            name="quantity"
+                            placeholder="Quantity"
+                            label="Quantity available"
+                            onChange={ updateNewListingForm }
+                        />
+                    </Form.Group>
                     <Divider />
                 </Segment>
             </Segment.Group>
+            <Segment secondary>
+                <Button primary>Create listing</Button>
+            </Segment>
         </Container>
     );
 
 }
 
 export default CreateListingPage;
+
+// <Dropdown
+//     name="unit"
+//     placeholder='Unit'
+//     clearable={ true }
+//     onChange={ updateNewListingForm }
+// >
+//     <Dropdown.Menu>
+//         <Dropdown.Header content='Unit' />
+//         <Dropdown.Item>each</Dropdown.Item>
+//         <Dropdown.Item>spool</Dropdown.Item>
+//         <Dropdown.Item>roll</Dropdown.Item>
+//         <Dropdown.Item>bolt</Dropdown.Item>
+//         <Dropdown.Divider />
+//         <Dropdown.Header content='Length' />
+//         <Dropdown.Item>inch</Dropdown.Item>
+//         <Dropdown.Item>yard</Dropdown.Item>
+//         <Dropdown.Item>centimeter</Dropdown.Item>
+//         <Dropdown.Item>meter</Dropdown.Item>
+//         <Dropdown.Divider />
+//         <Dropdown.Header content='Weight' />
+//         <Dropdown.Item>pound</Dropdown.Item>
+//         <Dropdown.Item>ounce</Dropdown.Item>
+//         <Dropdown.Item>gram</Dropdown.Item>
+//         <Dropdown.Item>kilogram</Dropdown.Item>
+//     </Dropdown.Menu>
+// </Dropdown>
